5a7a95f4eaefcb4e443a1fa130c752f2721d13bc
Jorge Rosano <jorg.ross.13@gmail.com>
2023-03-04 21:20:57 -0600

    add:checklists_methods

--- checklist.rb
+++ checklist.rb
@@ -6,13 +6,50 @@ class Checklist
    
   end
 
+  def show_checklist
 
-  # methods
+    # until option == "back"
+    #   print_card_checklist
+    #   puts "---------------------"
+    #   checklist_menu
+    # end
+
+  end
+
+  def add_checklist
+    print "Title: "
+    title = gets.chomp
+    { title: title, completed: false }
+    @checklist << @checklist
+  end 
+
+  def toggle_checklist(id)
+
+    card[:completed] = !card[:completed] if card
+  
+  end
+
+  def delete_checklist(id)
+    @checklist.delete_checklist(index - 1)
+  end
+  
+
+  def checklist_menu
+    puts "Checklist options: add | toggle INDEX | delete INDEX\nback"
+    print "> "
+    option, id = gets.chomp.split
+    [option, id.to_i]
+  end 
+
 end
-data =  {
-  title: "Add gem to gemfile",
-  completed: true
-}
-nuevo = Checklist.new(**data)
-p nuevo.title
-p nuevo.completed
+
+
+# data =  {
+#   title: "Add gem to gemfile",
+#   completed: true
+# }
+# nuevo = Checklist.new(**data)
+# p nuevo.show_checklist
+# p nuevo.add_checklist
+# p nuevo.toggle_checklist
+# p nuevo.delete_checklist
